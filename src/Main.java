9d2515f8eab97654a81ea381509cf707b21fc928
Kelly <kellygashi00@gmail.com>
2023-03-09 11:26:36 +0100

    Practice for getting an JSON file and print it on the console

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,24 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
 
-        Object o = new JSONParser().parse(new FileReader("C:\\Users\\kelly\\Desktop\\JSONPractice\\src\\data.json"));
+        String filepath = "C:\\Users\\kelly\\Desktop\\JSONPractice\\src\\data.json";
+
+        Object o = new JSONParser().parse(new FileReader(filepath));
         JSONObject jsonData = (JSONObject) o;
 
         JSONObject person = (JSONObject) jsonData.get("p1");
         String name = (String) person.get("name");
         int age = ((Long) person.get("age")).intValue();
 
+        JSONObject personTwo = (JSONObject) jsonData.get("p2");
+        String nameTwo = (String) personTwo.get("name");
+        int ageTwo = ((Long) personTwo.get("age")).intValue();
+
         System.out.println("P1 Name: " + name);
         System.out.println("P1 Age " + age);
+        System.out.println();
+        System.out.println("P2 Name: " + nameTwo);
+        System.out.println("P2 Age " + ageTwo);
 
     }
 }
